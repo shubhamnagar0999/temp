c9fe47f261ed5a6d3eced5b5a1c456a5b7e6560e
Shubham nagar <83903863+shubhamnagar0999@users.noreply.github.com>
2022-12-03 14:31:42 +0530

    Add files via upload

---
+++ home/external/js/index.js
@@ -0,0 +1,47 @@
+var handler = document.querySelector('.handler');
+var wrapper = handler.closest('.container-fluid');
+var boxA = wrapper.querySelector('.box');
+var isHandlerDragging = false;
+
+document.addEventListener('mousedown', function(e) {
+    if (e.target === handler) {
+      isHandlerDragging = true;
+    }
+  });
+
+  document.addEventListener('mousemove', function(e) {
+    // Don't do anything if dragging flag is false
+    if (!isHandlerDragging) {
+      return false;
+    }
+
+    var containerOffsetLeft = wrapper.offsetLeft; 
+
+    // Get x-coordinate of pointer relative to container
+    var pointerRelativeXpos = e.clientX - containerOffsetLeft;
+    
+    // Arbitrary minimum width set on box A, otherwise its inner content will collapse to width of 0
+    var boxAminWidth = 60;
+
+    // Resize box A
+    // * 8px is the left/right spacing between .handler and its inner pseudo-element
+    // * Set flex-grow to 0 to prevent it from growing
+    boxA.style.width = (Math.max(boxAminWidth, pointerRelativeXpos - 8)) + 'px';
+    boxA.style.flexGrow = 0;
+
+    document.addEventListener('mouseup', function(e) {
+        // Turn off dragging flag when user mouse is up
+        isHandlerDragging = false;
+    });
+
+   })
+
+   //<-- for view image -->
+   function upload(){
+    var imgcanvas = document.getElementById("canvas1");
+    var fileinput = document.getElementById("inputimg");
+    var image = new SimpleImage(fileinput);
+    image.drawTo(imgcanvas);
+  }
+
+  
